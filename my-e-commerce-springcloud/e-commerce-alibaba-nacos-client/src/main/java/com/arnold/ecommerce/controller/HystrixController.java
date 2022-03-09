331e466db262a7926dc08e886d0a4ead1743faa4
4rnold <arnold1989@sina.com>
2022-03-09 14:45:59 +0800

    spring-kafka

--- my-e-commerce-springcloud/e-commerce-alibaba-nacos-client/src/main/java/com/arnold/ecommerce/controller/HystrixController.java
+++ my-e-commerce-springcloud/e-commerce-alibaba-nacos-client/src/main/java/com/arnold/ecommerce/controller/HystrixController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 
 import com.arnold.ecommerce.service.NacosClientService;
 import com.arnold.ecommerce.service.communication.hystrix.*;
+import com.arnold.ecommerce.service.communication.hystrix.request_merge.NacosClientCollapseCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
